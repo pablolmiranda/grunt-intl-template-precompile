e8b180e4d623c9ac66fccf2b82e0b64ccc2cce2e
Pablo Lacerda de Miranda <pablolm@yahoo-inc.com>
2014-01-17 12:19:57 -0800

    adds clean and compile steps to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,32 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+
+        clean: {
+            tests: ['tmp']
+        },
+
         nodeunit: {
             all: ['test/test.js']
+        },
+
+        "intl-precompile-templates": {
+            dist: {
+                files: [{
+                    src: ['test/fixtures/*.pres'],
+                    templates: ['test/fixtures/*.mu'],
+                    dest: 'tmp/'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
+    grunt.loadTasks('tasks');
 
     grunt.registerTask('test', ['nodeunit']);
 
-    grunt.registerTask('default', ['test']);
+    grunt.registerTask('default', ['clean', 'intl-precompile-templates:dist', 'test']);
 }
